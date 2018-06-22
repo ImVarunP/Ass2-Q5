bb2598712fd399caf8c9aaa98746c7aa974d0654
ImVarunP <40432383+ImVarunP@users.noreply.github.com>
2018-06-22 19:39:19 +0530

    Initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styles: [`input{background:blue}`]
+})
+export class AppComponent {
+  name = `Marvellous`;
+}
